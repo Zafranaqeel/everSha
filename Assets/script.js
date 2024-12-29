b6a3e5fd29c01567d915a38ed0378ac1ac9a0d1d
Zafranaqeel <gitsprjct@gmail.com>
2024-12-29 16:18:47 +0700

    Update 2

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 function toggleMenu() {
     const hamburger = document.querySelector('.hamburger');
-    const menu = document.querySelector('.menu');
+    const navbar = document.querySelector('.navbar');
 
     hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
+    navbar.classList.toggle('active');
 }
